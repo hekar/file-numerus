9a595bd468d3237b13ff836315ef1e17cad3b7d3
Hekar Khani <hekark@gmail.com>
2021-05-15 02:52:19 -0400

    Added stub implementation for S3FileService

--- src/service/file-service/s3.ts
+++ src/service/file-service/s3.ts
@@ -1,5 +1,22 @@
 import { FileService } from "./file-service";
+import {
+  FileList,
+  FileEntry,
+  GetObjectOptions,
+  GetObjectResult,
+} from "./types";
 
 export class S3FileService implements FileService {
-
+  ls(_key: string): Promise<FileList> {
+    throw new Error("Method not implemented.");
+  }
+  getEntry(_key: string): Promise<FileEntry> {
+    throw new Error("Method not implemented.");
+  }
+  getObject(
+    _key: string,
+    _options?: GetObjectOptions
+  ): Promise<GetObjectResult> {
+    throw new Error("Method not implemented.");
+  }
 }
